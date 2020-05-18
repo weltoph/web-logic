4da7bbd516b46b88380788acb117d15b8ad8b173
Christoph Welzel <welzel@in.tum.de>
2020-05-18 21:23:11 +0200

    moved to production setting in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,21 @@ const path = require('path');
 module.exports = [
   {
     entry: './src/truthtable.js',
-    mode: "development",
+    mode: "production",
     output: {
       filename: 'truthtable.bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
   }, {
     entry: './src/syntaxtree.js',
-    mode: "development",
+    mode: "production",
     output: {
       filename: 'syntaxtree.bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
   }, {
-    entry: './src/fo-graph.js',
-    mode: "development",
+    entry: index: './src/fo-graph.js',
+    mode: "production",
     output: {
       filename: 'fo-graph.bundle.js',
       path: path.resolve(__dirname, 'dist'),
